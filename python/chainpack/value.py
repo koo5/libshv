f41871c2f93bc300d58008aa9f3aa99699414e08
Jindrich Kolman <jindrich.kolman@elektroline.cz>
2017-12-24 22:48:47 +0100

    wip

--- python/chainpack/value.py
+++ python/chainpack/value.py
@@ -6,6 +6,7 @@ import typing, types
 import logging_config
 import logging
 from copy import deepcopy
+from math import floor
 
 debug = logging.debug
 ARRAY_FLAG_MASK = 64
@@ -65,9 +66,55 @@ class Type(enum.IntFlag):
 	String=140
 	DateTime=141
 	List=142
-	Map=143
-	IMap=144
+	Array=143
+	Map=144
+	IMap=145
+	MetaIMap=146
 
+
+
+def typeToTypeInfo(type: Type):
+	if type == Type.INVALID:  raise Exception("There is no type info for type Invalid");
+	if type == Type.Array:    raise Exception("There is no type info for type Array");
+	if type == Type.Null:     return TypeInfo.Null;
+	if type == Type.UInt:     return TypeInfo.UInt;
+	if type == Type.Int:      return TypeInfo.Int;
+	if type == Type.Double:   return TypeInfo.Double;
+	if type == Type.Bool:     return TypeInfo.Bool;
+	if type == Type.Blob:     return TypeInfo.Blob;
+	if type == Type.String:   return TypeInfo.String;
+	if type == Type.List:     return TypeInfo.List;
+	if type == Type.Map:      return TypeInfo.Map;
+	if type == Type.IMap:     return TypeInfo.IMap;
+	if type == Type.DateTime: return TypeInfo.DateTime;
+	if type == Type.MetaIMap: return TypeInfo.MetaIMap;
+	raise Exception("Unknown RpcValue::Type!");
+
+def typeInfoToType(type_info: TypeInfo) -> Type:
+	if type_info == TypeInfo.Null:     return Type.Null;
+	if type_info == TypeInfo.UInt:     return Type.UInt;
+	if type_info == TypeInfo.Int:      return Type.Int;
+	if type_info == TypeInfo.Double:   return Type.Double;
+	if type_info == TypeInfo.Bool:     return Type.Bool;
+	if type_info == TypeInfo.Blob:     return Type.Blob;
+	if type_info == TypeInfo.String:   return Type.String;
+	if type_info == TypeInfo.DateTime: return Type.DateTime;
+	if type_info == TypeInfo.List:     return Type.List;
+	if type_info == TypeInfo.Map:      return Type.Map;
+	if type_info == TypeInfo.IMap:     return Type.IMap;
+	if type_info == TypeInfo.MetaIMap: return Type.MetaIMap;
+	raise Exception("There is no Type for TypeInfo %s"%(type_info));
+
+def chainpackTypeFromPythonType(t):
+	if t == type(None):  return Type.Null,
+	if t == bool:        return Type.Bool,
+	if t == int:         return Type.Int,
+	if t == float:       return Type.Double,
+	if t == datetime:    return Type.DateTime,
+	if t == str:         return Type.String,
+	if t == list:        return Type.List, Type.Array
+	if t == dict:        return Type.Map, Type.IMap
+	raise ChainpackTypeException("failed deducing chainpack type for python type %s"%t)
 
 class Tag(enum.IntEnum):
 	Invalid = 0
@@ -118,44 +165,33 @@ class RpcValue():
 		s.m_metaData[tag] = value
 	def isValid(s):
 		return s.m_type != Type.INVALID
-	def toInt(s):
+	def toInt(s) -> int:
 		assert s.m_type in [Type.Int, Type.UInt]
 		return s.m_value
-
-def chainpackTypeFromPythonType(t):
-		if t == type(None):    return Type.Null,
-		elif t == bool:      return Type.Bool,
-		elif t == int:         return Type.Int,
-		elif t == float:         return Type.Double,
-		elif t == datetime:         return Type.DateTime,
-		elif t == str:         return Type.String,
-		elif t == list:         return Type.List, Type.Array
-		elif t == dict:         return Type.Map, Type.IMap
-		else:
-			raise ChainpackTypeException("failed deducing chainpack type for python type %s"%t)
+	def toBool(s) -> bool:
+		assert s.m_type in [Type.Bool]
+		return s.m_value
 
 def optimizedMetaTagType(tag: Tag) -> TypeInfo:
 	if tag == Tag.MetaTypeId: return TypeInfo.META_TYPE_ID;
 	if tag == Tag.MetaTypeNameSpaceId: return TypeInfo.META_TYPE_NAMESPACE_ID;
 	return TypeInfo.INVALID;
 
-def optimizeRpcValueIntoType(pack: RpcValue) -> (bool, TypeInfo):
+def optimizeRpcValueIntoType(pack: RpcValue) -> int:
 		if (not pack.isValid()):
 			raise ChainpackTypeException("Cannot serialize invalid ChainPack.");
 		t = pack.m_type# type: Type
 		if(t == Type.Bool) :
-			return True, [TypeInfo.FALSE, TypeInfo.TRUE][pack.toBool()]
+			return [TypeInfo.FALSE, TypeInfo.TRUE][pack.toBool()]
 		elif t == Type.UInt:
-			n = pack.toInt() # type: Int
+			n = pack.toInt() # type: int
 			if n in range(64):
 				return n
 		elif t == Type.Int:
-			n = pack.toInt() # type: Int
+			n = pack.toInt()
 			if n in range(64):
 				return n + 64
-		elif(type == Type.Array):
-			t = typeToTypeInfo(pack.arrayType)
-			return t | ARRAY_FLAG_MASK
+		return None
 
 class Blob(bytearray):
 	DOUBLE_FMT = '!d'
@@ -191,12 +227,23 @@ class Blob(bytearray):
 		if t != None:
 			out.append(t)
 		else:
-			out.append(value.m_type)
+			if(value.m_type == Type.Array):
+				t = typeToTypeInfo(pack.arrayType) | ARRAY_FLAG_MASK
+			else:
+				t = typeToTypeInfo(value.m_type)
+			out.append(t)
 			out.writeData(value)
 		return out
 
 	def writeMetaData(s, md: MetaData):
-		imap = RpcValue({}, TypeInfo.IMap)
+		imap = RpcValue(md, TypeInfo.IMap)
+		if len(imap):
+			s.append(TypeInfo.MetaIMap)
+			s.writeData_IMap(imap.value);
+
+
+	"""
+	
 		for key in md.keys():
 			type = optimizedMetaTagType(key)
 			if(type != TypeInfo.INVALID):
@@ -206,17 +253,16 @@ class Blob(bytearray):
 				assert(val >= 0 and val <= 255)
 				s.write_UIntData(val)
 			else:
-				imap[key] = md[(key)]
+				imap.value[key] = md[key]
 		if len(imap):
-			append(TypeInfo.MetaIMap)
-			writeData_IMap(imap);
+	"""
 
 	def writeData(s, val: RpcValue):
 		v = val.m_value
 		t = val.m_type # type: Type
 		if   t == Type.Null:    return
 		elif t == Type.Bool:     s.append([b'0', b'1'][v])
-		elif t == Type.UInt:     s.write_fmt(s.UINT_FMT, v)
+		elif t == Type.UInt:     s.write_UIntData(v)
 		elif t == Type.Int:      s.write_fmt(s.INT_FMT, v)
 		elif t == Type.Double:   s.write_fmt(s.DOUBLE_FMT, v)
 		elif t == Type.DateTime: s.write_DateTime(v)
@@ -226,30 +272,47 @@ class Blob(bytearray):
 		elif t == Type.Array:    s.writeData_Array(v)
 		elif t == Type.Map:      s.writeData_Map(v)
 		elif t == Type.IMap:     s.writeData_IMap(v)
-		elif t == Type.Invalid:  raise ChainpackTypeException("Internal error: attempt to write invalid type data")
+		elif t == Type.INVALID:  raise ChainpackTypeException("Internal error: attempt to write invalid type data")
 		elif t == Type.MetaIMap:	raise ChainpackTypeException("Internal error: attempt to write metatype directly")
+
+	def write_UIntData(s, v: int):
+		s.write_fmt(s.UINT_FMT, v)
+
+	def write_Blob(s, v):
+		b = v
+		if type(v) == str:
+			b = v.encode('utf-8')
+		else:
+			assert type(v) == bytearray
+		write_UIntData(len(b))
+		for i in b:
+			s.append(i)
+
+	def write_DateTime(s, v):
+		s.write_IntData(floor(v.timestamp()*1000))
 
 	def pop_front(s, size: int):
 		for i in range(size):
 			s.pop(0)
 
 	def read_UIntData(s):
-		return s.read_fmt(FMT_UINT)
+		return s.read_fmt(s.FMT_UINT)
 
 	def readData_IMap(s) -> RpcValue:
 		ret = RpcValue({}, Type.IMap)
-		int: map_size = s.read_UIntData()
+		map_size: int = s.read_UIntData()
 		for i in range(map_size):
 			key = s.read_UIntData()
-			ret[key] = s.read()
+			ret.value[key] = s.read()
 		return ret
 
-	def writeData_IMap(RpcValue: map) -> None:
-		assert map.type == Type.IMap
-		write_fmt(FMT_UINT, len(map))
+	def writeData_IMap(s, map: dict) -> None:
+		assert type(map) == dict
+		#write_fmt(FMT_UINT, len(map))
 		for k, v in map.items():
-			write_fmt(FMT_UINT, k)
-			write(v)
+			s.write_fmt(s.FMT_UINT, k)
+			s.write(v)
+		s.append(TypeInfo.TERM)
 
 	def read_fmt(s, fmt):
 		size = struct.calcsize(fmt)
@@ -296,53 +359,53 @@ class Blob(bytearray):
 	def peek(s):
 		return s[0]
 
-	def readMetaData(s) -> dict:
+	def readMetaData(s) -> MetaData:
 		ret = MetaData()
 		while True:
 			type_info: int = s.peek();
 			if type_info == TypeInfo.META_TYPE_ID:
 				s.pop(0)
-				uint: u = s.read_UIntData();
+				u: int = s.read_UIntData();
 				ret[Tag.MetaTypeId] = u
 			elif type_info == TypeInfo.META_TYPE_NAMESPACE_ID:
 				s.pop(0)
-				uint: u = s.read_UIntData();
+				u = s.read_UIntData();
 				ret[Tag.MetaTypeNameSpaceId] = u
 			elif type_info == TypeInfo.MetaIMap:
 				s.pop(0)
-				ret += s.readData_IMap()
+				for k,v in s.readData_IMap().value.items():
+					ret[k] = v
 			else:
 				break;
 		return ret;
 
-	def readTypeInfo(s) -> (TypeInfo, RpcValue, int):
-		int: t = s.get()
-		meta = None
-		if(t & 128):
-			t = t & ~128;
-			meta = s.read();
-		if(t >= 64):
-			return TypeInfo.UInt, meta, t - 64;
-		else: return t, meta
+#	def readTypeInfo(s) -> (TypeInfo, RpcValue, int):
+#		t: int = s.get()
+#		meta = None
+#		if(t & 128):
+#			t = t & ~128;
+#			meta = s.read();
+#		if(t >= 64):
+#			return TypeInfo.UInt, meta, t - 64;
+#		else: return t, meta
 
 	def readData(s, t: TypeInfo, is_array: bool) -> RpcValue:
-		RpcValue: ret;
 		if(is_array):
-			list: val = s.readData_Array(type);
-			ret = RpcValue(val);
+			val: list = s.readData_Array(t);
+			return RpcValue(val);
 		else:
 			if   t == TypeInfo.Null:     return RpcValue(None)
-			elif t == TypeInfo.UInt:     return RpcValue(s.read_UIntData(data))
-			elif t == TypeInfo.Int:      return RpcValue(s.read_IntData(data))
-			elif t == TypeInfo.Double:   return RpcValue(s.read_Double(data))
+			elif t == TypeInfo.UInt:     return RpcValue(s.read_UIntData())
+			elif t == TypeInfo.Int:      return RpcValue(s.read_IntData())
+			elif t == TypeInfo.Double:   return RpcValue(s.read_Double())
 			elif t == TypeInfo.TRUE:     return RpcValue(True)
 			elif t == TypeInfo.FALSE:    return RpcValue(False)
-			elif t == TypeInfo.DateTime: return RpcValue(s.read_DateTime(data))
-			elif t == TypeInfo.String:   return RpcValue(s.read_Blob(data))
-			elif t == TypeInfo.Blob:     return RpcValue(s.read_Blob(data))
-			elif t == TypeInfo.List:     return RpcValue(s.readData_List(data))
-			elif t == TypeInfo.Map:      return RpcValue(s.readData_Map(data))
-			elif t == TypeInfo.IMap:     return RpcValue(s.readData_IMap(data), TypeInfo.IMap)
+			elif t == TypeInfo.DateTime: return RpcValue(s.read_DateTime())
+			elif t == TypeInfo.String:   return RpcValue(s.read_Blob())
+			elif t == TypeInfo.Blob:     return RpcValue(s.read_Blob())
+			elif t == TypeInfo.List:     return RpcValue(s.readData_List())
+			elif t == TypeInfo.Map:      return RpcValue(s.readData_Map())
+			elif t == TypeInfo.IMap:     return RpcValue(s.readData_IMap(), TypeInfo.IMap)
 			elif t == TypeInfo.Bool:     return RpcValue(s.get() != b'\0')
 			else: raise	ChainpackTypeException("Internal error: attempt to read meta type directly. type: " + str(t) + " " + t.__name__)
 
